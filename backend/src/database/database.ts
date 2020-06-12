286b5f4dfd4bcce28510194d7d679066110cb8fa
satisf <ppmoeller@posteo.net>
2020-06-12 14:36:48 +0200

    adds missing backend changes

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -5,7 +5,9 @@ let database: Mongoose.Connection;
 export const connect = () => {
     // add your own uri below
  //    const uri = "mongodb+srv://admin:fYySVZVOjbb52wlG@cluster0-seslf.mongodb.net/zaster?retryWrites=true&w=majority";
-    const uri = "mongodb://127.0.0.1:27017/zaster"
+    const uri = process.env.ENV === 'DEV' ?
+        "mongodb+srv://admin:fYySVZVOjbb52wlG@cluster0-seslf.mongodb.net/zaster?retryWrites=true&w=majority" :
+        "mongodb://127.0.0.1:27017/zaster"
     if (database) {
         return;
     }
--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,13 +1,18 @@
-import express from "express";
+import express, {ErrorRequestHandler, NextFunction, RequestHandler} from "express";
 import { connect } from "../database/database";
 import {PurchaseModel} from "../database/purchases/purchases.model";
+const path = require('path');
 
 
 const app = express();
 
 app.use(express.json())
+app.use((req, res, next) => {
+    console.log('incoming: ', req.originalUrl)
+    next()
+});
 
-const port = 5002;
+const port = process.env.PORT || 80;
 connect();
 
 app.post('/api/purchase', (req, res) => {
@@ -39,7 +44,9 @@ app.get('/api/purchases', (req, res) => {
 app.delete('/api/purchase/:purchaseId/delete', (req, res) => {
     PurchaseModel.deleteOne({_id: req.params.purchaseId}).then(() => res.send("ok"), () => res.send("error"))
 })
-
+app.get('*', (req,res) =>{
+    res.sendFile('index.html', {root: path.join(__dirname+'/../../../frontend/build')});
+});
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
